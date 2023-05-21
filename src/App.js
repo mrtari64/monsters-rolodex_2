ecaa3e0d4c65051ddd876b68c77e8dcda39eec0d
tari <sds@dfd.com>
2023-05-21 13:59:31 +0330

    app.js--its about components state-rendering and rerendering of component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    console.log("constructor");
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            console.log("111111");
+            return { monsters: users };
+          },
+          () => console.log(this.state)
+        );
+      });
+  }
+  render() {
+    console.log("render");
+    return (
+      <div className="App">
+        {this.state.monsters.map((m) => {
+          return (
+            <div key={m.id}>
+              <h1>{m.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
